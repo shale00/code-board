60e104ba2f9f9a707e8b6ca8024d6ac6a9340f10
cjsolis <cjelizondo12@gmail.com>
2023-05-12 11:31:24 -0500

    added seed.js to include posts.json

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,22 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Posts } = require('../models');
 
 const userData = require('./userData.json');
+const postsData = require('./posts.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+  for (const post of postsData) {
+    await Posts.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };

9a0fa4001b33b8b9b2e5a986f2665a5f84d187db
cjsolis <cjelizondo12@gmail.com>
2023-05-12 10:59:42 -0500

    added the structure for posts model

--- models/Posts.js
+++ models/Posts.js
@@ -16,37 +16,28 @@ Posts.init(// changed to posts
       primaryKey: true,
       autoIncrement: true,
     },
-    user_id: {
-      type: DataTypes.INTEGER,
+    title: {
+      type: DataTypes.STRING,
       allowNull: false,
       // foreign key
       // post content 
       // content 
       // above would replace below.
     },
-    email: {
+    description: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
       },
     },
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,

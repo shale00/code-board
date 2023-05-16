4b8b739fc82350cc5ef2125ab5bc3fdb84a7bac2
cjsolis <cjelizondo12@gmail.com>
2023-05-16 11:58:25 -0500

    fixed types on model forms

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,18 +3,20 @@ const signupFormHandler = async (event) => {
 
     const name = document.querySelector('#name4').value.trim();
     const email = document.querySelector('#email4').value.trim();
-    const gitHub = document.querySelector('#github').value.trim();
+    const github = document.querySelector('#github').value.trim();
     const linkedin = document.querySelector('#linkedIn').value.trim();
     const password = document.querySelector('#password').value.trim();
 
-    if(name && password && email && gitHub && linkedin ) {
+    if(name && password && email && github && linkedin ) {
         const response = await fetch ('api/user/register', {
            method: 'POST',
            body: JSON.stringify({ name, password, email, linkedin, github }),
            headers: { 'Content-Type': 'application/json' },
         });
+        console.log(name, password, email, github, linkedin);
 
         if(response.ok){
+            alert(`Created a new account!`)
             document.location.replace('/');
         } else {
             alert(`missing information`)

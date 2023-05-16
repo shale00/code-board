560e0aca8538b4986e86479ecde26263359cdefe
Tim Mackie <timmackie177@gmail.com>
2023-05-15 19:51:08 -0500

    updated the email naming in the json

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 
-router.use('/users', userRoutes);
-router.use('/', postRoutes);
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
 
 
 module.exports = router;

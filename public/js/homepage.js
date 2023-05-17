24ff874136eaf19115e471603392c65f9a6dfe66
sth <sth0067@gmail.com>
2023-05-16 23:20:12 -0500

    added createpost.js, updated handlebars and homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -16,54 +16,63 @@ showMoreButton.addEventListener('click', function () {
     currentPosts += increment;
     window.location.href= `/${currentPosts}`;
     console.log(currentPosts);
-    displayPosts();
+    // displayPosts();
 });
 
-// Event listener for "New Post" button
-newPostButton.addEventListener('click', function () {
-    newPostModal.style.display = 'block';
-});
+// // Event listener for "New Post" button
+// newPostButton.addEventListener('click', function () {
+//     newPostModal.style.display = 'block';
+// });
 
-// Event listener for closing the modal
-closeModal.addEventListener('click', function () {
-    newPostModal.style.display = 'none';
-});
+// // Event listener for closing the modal
+// closeModal.addEventListener('click', function () {
+//     newPostModal.style.display = 'none';
+// });
+
+  //Modal listener TODO: This is not doing anything
+//   document.getElementById("newPostButton").addEventListener("click", function() {
+//     // Get the modal element
+//     var modal = document.getElementById("newPostModal");
+  
+//     // Show the modal
+//     var modalBootstrap = new bootstrap.Modal(modal);
+//     modalBootstrap.show();
+//   });
 
 
-const newPost = async (event) => {
-    event.preventDefault();
+// const newPostFormHandler = async (event) => {
+//     event.preventDefault();
 
-    // const title = document.querySelector('#title').value.trim();
-    const body = document.querySelector('#content').value.trim();
+//     const body = document.querySelector('#content').value.trim();
 
-    if(title && content ) {
-        const response = await fetch ('api/post/create', {
-           method: 'POST',
-           body: JSON.stringify({ body }),
-           headers: { 'Content-Type': 'application/json' },
-        });
+//     if( body ) {
+//         const response = await fetch ('api/post/create', {
+//            method: 'POST',
+//            body: JSON.stringify({ body }),
+//            headers: { 'Content-Type': 'application/json' },
+//         });
 
-        if(response.ok){
-            alert(`Created a new post!`)
-            document.location.replace('/');
-        } else {
-            alert(`something is wrong`)
-        }
-    }
+//         if(response.ok){
+//             alert(`Created a new post!`)
+//             document.location.replace('/');
+//         } else {
+//             alert(`something is wrong`)
+//         }
+//     }
 
-     // Reset the form
-     newPostForm.reset();
+//      // Reset the form
+//      newPostForm.reset();
 
-     // Close the modal
-     newPostModal.style.display = 'none';
+//      // Close the modal
+//      newPostModal.style.display = 'none';
  
-     // Update the displayed posts
-     displayPosts();
+//      // Update the displayed posts
+//      displayPosts();
     
-}
+// }
 
-const create = document
-  .querySelector('#createPostSubmit') 
-create
-  .addEventListener('click', (e) => newPost(e));
+// const create = document
+//   .querySelector('#createPostSubmit') 
+// create
+//   .addEventListener('click', (e) => newPost(e));
 
